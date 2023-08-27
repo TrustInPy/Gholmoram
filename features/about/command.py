910853bf569a93aa2113794c4d92c289a3738a9b
TrustInPy <sinasolimany@gmail.com>
2023-08-27 20:22:54 +0330

    Alternative text for Admin mention

--- features/about/command.py
+++ features/about/command.py
@@ -1,6 +1,13 @@
-from bot import *
+from bot import client
 from version import VERSION
 from telethon.sync import events
+from telethon import functions, types
+
+ADMIN = {
+    "name": "Sina",
+    "access_hash": -4572404436797027872,
+    "user_id": 1465986382,
+}
 
 
 @client.on(
@@ -9,7 +16,35 @@ from telethon.sync import events
 async def about(event):
     message_chat_id = event.chat_id
     try:
-        about = f"**🇮🇷 Gholmoram** \n✅ **v{VERSION}** \n📌 **AEDAN GAMING** \n👨🏻‍💻 {ADMIN_USERNAME} \n🖥 [Github](https://github.com/aedangaming) \n💬 [Discord](https://discord.gg/ZJVhgBCw3Q)"
+        users = None
+        try:
+            users = await client(
+                functions.users.GetUsersRequest(
+                    [types.InputUser(ADMIN["user_id"], ADMIN["access_hash"])]
+                )
+            )
+        except Exception as e:
+            print(e)
+            pass
+
+        if users and len(users) > 0:
+            about = (
+                "**🇮🇷 Gholmoram** \n"
+                + f"✅ **v{VERSION}** \n"
+                + "📌 **AEDAN GAMING** \n"
+                + f"👨🏻‍💻 [{ADMIN['name']}](tg://resolve/?domain={users[0].username})\n"
+                + "🖥 [Github](https://github.com/aedangaming) \n"
+                + "💬 [Discord](https://discord.gg/ZJVhgBCw3Q)"
+            )
+        else:
+            about = (
+                "**🇮🇷 Gholmoram** \n"
+                + f"✅ **v{VERSION}** \n"
+                + "📌 **AEDAN GAMING** \n"
+                + f"👨🏻‍💻 {ADMIN['name']}\n"
+                + "🖥 [Github](https://github.com/aedangaming) \n"
+                + "💬 [Discord](https://discord.gg/ZJVhgBCw3Q)"
+            )
         await client.send_message(message_chat_id, about)
     except Exception as e:
-        print("About" + str(e))
+        print("About " + str(e))

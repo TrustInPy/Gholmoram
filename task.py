7f80f70bf5dc06913dbd9bb95c98e542113622a7
TrustInPy <sinasolimany@gmail.com>
2023-09-15 10:19:17 +0330

    Inserting chats data to table added to Tasks

--- task.py
+++ task.py
@@ -1,16 +1,18 @@
 import asyncio
 import aiosqlite
 from data.database import DATABASE_NAME, USER_DATA_CACHE, insert_or_update_all_users
+from data.chats_data import CHAT_DATA_CACHE, insert_or_update_all_chats
 
 
 async def update_database_periodically():
     while True:
-        if len(USER_DATA_CACHE) == 0:
+        if len(USER_DATA_CACHE) == 0 and len(CHAT_DATA_CACHE) == 0:
             await asyncio.sleep(40)
         else:
             try:
-                conn = await aiosqlite.connect(DATABASE_NAME)
+                conn = await aiosqlite.connect(DATABASE_NAME, timeout=10)
                 await insert_or_update_all_users(conn, USER_DATA_CACHE)
+                await insert_or_update_all_chats(conn, CHAT_DATA_CACHE)
                 await conn.close()
                 await asyncio.sleep(40)
             except Exception as e:

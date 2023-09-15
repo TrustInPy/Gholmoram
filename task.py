0d6a7b2f961048f254e149710acec6e4878f436c
TrustInPy <sinasolimany@gmail.com>
2023-09-15 07:32:55 +0330

    Update database task added

--- task.py
+++ task.py
@@ -1,5 +1,21 @@
 import asyncio
+import aiosqlite
+from data.database import DATABASE_NAME, USER_DATA_CACHE, insert_or_update_all_users
+
+
+async def update_database_periodically():
+    while True:
+        if len(USER_DATA_CACHE) == 0:
+            await asyncio.sleep(40)
+        else:
+            try:
+                conn = await aiosqlite.connect(DATABASE_NAME)
+                await insert_or_update_all_users(conn, USER_DATA_CACHE)
+                await conn.close()
+                await asyncio.sleep(40)
+            except Exception as e:
+                print(e)
 
 
 async def starter():
-    asyncio.gather()
+    asyncio.create_task(update_database_periodically())

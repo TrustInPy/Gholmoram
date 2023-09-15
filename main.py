dacf84ca8cbb28c3242056f6362f19ad9db71d50
TrustInPy <sinasolimany@gmail.com>
2023-09-15 07:32:02 +0330

    Run database at start Run tasks at start

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-import features
 import event
+import asyncio
+import features
 from bot import client
 from task import starter
+from data.database import run_database
+
+
+# Run the database setup asynchronously
+async def setup_database():
+    await run_database()
+
+
+# Start the bot and run database setup
+async def main():
+    await setup_database()
+    await starter()
+
+    print("--------------------------------------------------------")
+    print("Database ready +++")
+
+
+# Start the bot client and other setup
+client.loop.run_until_complete(main())
 
 client.start()
+print("--------------------------------------------------------")
 print("Bot started... ")
 
-client.loop.run_until_complete(starter())
 client.run_until_disconnected()

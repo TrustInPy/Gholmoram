087acfc98c21c84debe0e1d62205a7c549ec96e7
TrustInPy <sinasolimany@gmail.com>
2023-09-16 18:53:54 +0330

    Task edited to close connection after database updates

--- task.py
+++ task.py
@@ -1,7 +1,7 @@
 import asyncio
 import aiosqlite
-from data.database import DATABASE_NAME, USER_DATA_CACHE, insert_or_update_all_users
 from data.chats_data import CHAT_DATA_CACHE, insert_or_update_all_chats
+from data.database import DATABASE_NAME, USER_DATA_CACHE, insert_or_update_all_users
 
 
 async def update_database_periodically():
@@ -10,13 +10,15 @@ async def update_database_periodically():
             await asyncio.sleep(40)
         else:
             try:
-                conn = await aiosqlite.connect(DATABASE_NAME, timeout=10)
+                conn = await aiosqlite.connect(DATABASE_NAME)
                 await insert_or_update_all_users(conn, USER_DATA_CACHE)
                 await insert_or_update_all_chats(conn, CHAT_DATA_CACHE)
-                await conn.close()
-                await asyncio.sleep(40)
             except Exception as e:
                 print(e)
+            finally:
+                if conn is not None:
+                    await conn.close()
+            await asyncio.sleep(40)
 
 
 async def starter():

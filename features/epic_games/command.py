2b6451262a7ebfe104c4228e28aad07cecef2ced
TrustInPy <sinasolimany@gmail.com>
2023-09-29 17:42:35 +0330

    Epic command fixed to return valid free games from DB

--- features/epic_games/command.py
+++ features/epic_games/command.py
@@ -6,24 +6,31 @@ from datetime import datetime
 
 @client.on(events.NewMessage(pattern="(?i)/epic"))
 async def handler(event):
-    conn = await aiosqlite.connect(DATABASE_NAME)
+    try:
+        conn = await aiosqlite.connect(DATABASE_NAME)
 
-    cursor = await conn.cursor()
-    current_date = datetime.now()
-    query = f"""
-        SELECT * FROM free_games
-        WHERE end_date >= ?
-        """
-    await cursor.execute(query, (current_date,))
-    free_games = await cursor.fetchall()
-    await conn.close()
+        cursor = await conn.cursor()
+        current_date = datetime.now()
+        query = f"""
+            SELECT * FROM free_games
+            WHERE end_date >= ?
+            """
+        await cursor.execute(query, (current_date,))
+        free_games = await cursor.fetchall()
+        await conn.close()
 
-    for game in free_games:
-        game_name = game[1]
-        end_date = datetime.strptime(game[2], "%Y-%m-%d %H:%M:%S")
-        game_link = game[3]
+        for game in free_games:
+            game_name = game[1]
+            end_date = datetime.strptime(game[2], "%Y-%m-%d %H:%M:%S")
+            game_link = game[3]
 
-        end_date_str = end_date.strftime("%Y-%m-%dT%H:%M")
+            end_date_str = end_date.strftime("%Y-%m-%dT%H:%M")
 
-        message = f"{game_name} is free until {end_date_str}. Get it here: {game_link}"
-        await client.send_message(event.chat_id, message)
+            message = (
+                f"{game_name} is free until {end_date_str}. Get it here: {game_link}"
+            )
+            await client.send_message(event.chat_id, message)
+
+    except Exception as e:
+        print("Failed to get free games from database.\n" + str(e))
+        await client.send_message(event.chat_id, "خطا در دریافت لینک‌ها ⚠️")

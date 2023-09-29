d614ae9bb3ebc58df1e55d8a45c49428291b58c7
TrustInPy <sinasolimany@gmail.com>
2023-09-29 17:40:51 +0330

    Both tasks gathered Epic cache updater deleted

--- task.py
+++ task.py
@@ -1,36 +1,33 @@
 import asyncio
 import aiosqlite
+from data.epic_game_data import get_free_games_links
 from data.chats_data import CHAT_DATA_CACHE, insert_or_update_all_chats
 from data.database import DATABASE_NAME, USER_DATA_CACHE, insert_or_update_all_users
-from data.epic_game_data import FREE_GAMES_CACHE, insert_or_update_all_free_games, get_free_games_links
 
 
 async def update_database_periodically():
     while True:
-        if len(USER_DATA_CACHE) == 0 and len(CHAT_DATA_CACHE) == 0:
-            await asyncio.sleep(20)
-        else:
-            try:
-                conn = await aiosqlite.connect(DATABASE_NAME)
-                await insert_or_update_all_users(conn, USER_DATA_CACHE)
-                await insert_or_update_all_chats(conn, CHAT_DATA_CACHE)
-                await insert_or_update_all_free_games(conn, FREE_GAMES_CACHE)
-            except Exception as e:
-                print(e)
-            finally:
-                if conn is not None:
-                    await conn.close()
-            await asyncio.sleep(20)
+        try:
+            conn = await aiosqlite.connect(DATABASE_NAME)
+            await insert_or_update_all_users(conn, USER_DATA_CACHE)
+            await insert_or_update_all_chats(conn, CHAT_DATA_CACHE)
+        except Exception as e:
+            print(e)
+        finally:
+            if conn is not None:
+                await conn.close()
+        await asyncio.sleep(600)
 
 
-async def hour_task():
-    try:
-        await get_free_games_links()
-        await asyncio.sleep(20)
-    except Exception as e:
-        print(e)
+async def tasks():
+    while True:
+        try:
+            await get_free_games_links()
+            print("Epic free games updated in database.")
+            await asyncio.sleep(300)
+        except Exception as e:
+            print(e)
 
 
 async def starter():
-    asyncio.create_task(update_database_periodically())
-    asyncio.create_task(hour_task())
+    asyncio.gather(update_database_periodically(), tasks())

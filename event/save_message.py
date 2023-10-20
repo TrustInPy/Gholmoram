7eccc59225c7e24133c1b2ede7dfac8e98ed94ed
TrustInPy <sinasolimany@gmail.com>
2023-10-20 21:46:00 +0330

    Save_message now handles anonymous members messages

--- event/save_message.py
+++ event/save_message.py
@@ -1,51 +1,57 @@
 import aiosqlite
 from telethon.sync import events
 from bot import client, DATABASE_NAME
+from telethon.tl.types import PeerUser, PeerChannel
 
 
 @client.on(events.NewMessage())
 async def handler(event):
     user = await event.get_sender()
-    user_id = user.id
+    if user == None:
+        user_id = event.message.peer_id.channel_id
+    else:
+        user_id = user.id
     chat_id = event.chat_id
     message_id = event.id
     reply_to_message_id = event.reply_to_msg_id
     if event.message.forward:
-        if event.message.forward.from_id:
-            forward_from_user_id =  event.message.forward.from_id.user_id
+        if type(event.message.forward.from_id) is PeerUser:
+            forward_from_user_id = event.message.forward.from_id.user_id
+        elif type(event.message.forward.from_id) is PeerChannel:
+            forward_from_user_id = event.message.forward.from_id.channel_id
         else:
-            forward_from_user_id = event.message.forward.from_name
+            forward_from_user_id = "Unknown"
     else:
         forward_from_user_id = None
-
 
     if event.photo:
         if event.message.message:
             message_text = f"<Photo>\n {event.message.message}"
-        else :
+        else:
             message_text = "<Photo>"
-            
+
     elif event.audio:
         if event.message.message:
             message_text = f"<Audio>\n {event.message.message}"
-        else :
+        else:
             message_text = "<Audio>"
 
     elif event.video:
         if event.message.message:
             message_text = f"<Video>\n {event.message.message}"
-        else :
+        else:
             message_text = "<Video>"
 
     elif event.document:
         if event.message.message:
-            message_text = f"<Document ({event.document.mime_type})>\n {event.message.message}"
-        else :
+            message_text = (
+                f"<Document ({event.document.mime_type})>\n {event.message.message}"
+            )
+        else:
             message_text = f"<Document ({event.document.mime_type})>"
 
     else:
         message_text = event.raw_text
-
 
     try:
         conn = await aiosqlite.connect(DATABASE_NAME)
@@ -55,11 +61,17 @@ async def handler(event):
             INSERT INTO messages (chat_id, user_id, message_id, message_text, reply_to_message_id, forward_from_user_id)
             VALUES (?, ?, ?, ?, ?, ?)
             """,
-            (chat_id, user_id, message_id, message_text, reply_to_message_id, forward_from_user_id)
+            (
+                chat_id,
+                user_id,
+                message_id,
+                message_text,
+                reply_to_message_id,
+                forward_from_user_id,
+            ),
         )
         await conn.commit()
         print("Message saved.")
         await conn.close()
     except aiosqlite.Error as e:
         print(f"Error saving message: {e}")
-

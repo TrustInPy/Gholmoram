338d220739aaa11c9987bb4820b12d9c1054a488
TrustInPy <sinasolimany@gmail.com>
2024-03-19 18:18:21 +0330

    FIxed some problems in notification

--- features/notification/command.py
+++ features/notification/command.py
@@ -1,7 +1,6 @@
 import aiosqlite
 from telethon.sync import events, Button
 from bot import client, DATABASE_NAME, ADMIN_ID
-from datetime import datetime
 
 notification = None
 selected_chats = []
@@ -20,24 +19,29 @@ async def handler(event):
     chats = [(row[0], row[1]) for row in result]
     await connection.close()
     if event.sender_id == ADMIN_ID:
+
         try:
-            notification = event.raw_text.replace("/notification", "").strip()
-        except:
-            await client.send_message(event.chat_id, "متن پیام دریافت نشد 💤")
-        selected_chats = []
-        buttons = [
-            [Button.inline(chat[1], data=chat[0])]
-            for chat in chats
-            if chat not in selected_chats
-        ]
-        buttons.append([Button.inline("📨 ارسال", data="finish")])
-        buttons.append([Button.inline("❌ حذف همه", data="deselect")])
-        button_message = await client.send_message(
-            event.chat_id,
-            "🔅لطفا گروه های مورد نظر را انتخاب کنید: \n\nگروه ها: \n"
-            + "\n".join([chat[1] for chat in selected_chats]),
-            buttons=buttons,
-        )
+            notification = event.message.text.split(" ")
+            if len(notification) < 2:
+                await client.send_message(event.chat_id, "متن پیام دریافت نشد 💤")
+                return
+
+            selected_chats = []
+            buttons = [
+                [Button.inline(chat[1], data=chat[0])]
+                for chat in chats
+                if chat not in selected_chats
+            ]
+            buttons.append([Button.inline("📨 ارسال", data="finish")])
+            buttons.append([Button.inline("❌ حذف همه", data="deselect")])
+            button_message = await client.send_message(
+                event.chat_id,
+                "🔅لطفا گروه های مورد نظر را انتخاب کنید: \n\nگروه ها: \n"
+                + "\n".join([chat[1] for chat in selected_chats]),
+                buttons=buttons,
+            )
+        except Exception as e:
+            print("Error in /notification" + str(e))
 
 
 @client.on(events.CallbackQuery)
@@ -45,12 +49,12 @@ async def callback_handler(event):
     global notification
     global selected_chats
     if event.sender_id == ADMIN_ID:
-        # Get the selected chat or 'finish' or 'deselect'
+
         selection = event.data.decode("utf-8")
         if selection == "finish":
-            # Send the notification to the selected chats
+
             for chat in selected_chats:
-                await client.send_message(chat[0], notification)
+                await client.send_message(chat[0], notification[1])
             try:
                 await client.delete_messages(event.chat_id, button_message)
             except Exception as e:
@@ -59,18 +63,29 @@ async def callback_handler(event):
             selected_chats = []
             return
         elif selection == "deselect":
+            try:
+                await client.delete_messages(event.chat_id, button_message)
+            except Exception as e:
+                pass
             selected_chats = []
+            notification = None
+            return
         else:
-            # Convert selection to int if possible
             try:
                 selection = int(selection)
             except ValueError:
-                pass  # selection is not an integer, do nothing
-            # Find the selected chat by its id and add it to the list
-            selected_chat = next((chat for chat in chats if chat[0] == selection), None)
+                pass
+            try:
+                selected_chat = next(
+                    (chat for chat in chats if chat[0] == selection), None
+                )
+            except Exception as e:
+                await client.send_message(event.chat_id, "دوباره سعی کنید ⛔")
+                await client.delete_messages(event.chat_id, event.message_id)
+                return
             if selected_chat is not None:
                 selected_chats.append(selected_chat)
-        # Edit the message with the updated selected chats
+
         await event.edit(
             "🔅لطفا گروه های مورد نظر را انتخاب کنید: \n\nگروه ها: \n"
             + "\n".join([chat[1] for chat in selected_chats]),

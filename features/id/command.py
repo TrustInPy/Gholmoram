428dfd6805617451bcffe2a6d0735d9b4ab6c867
m-ehsan <m_ehsan_sh@live.com>
2025-04-07 12:47:16 +0330

    /id now uses InputPeer to get user IDs of mentioned usernames This allows to get IDs for channels but it has lost the ability to distinguish bots

--- features/id/command.py
+++ features/id/command.py
@@ -10,8 +10,6 @@ async def handle_new_message(event: telethon.events.NewMessage.Event):
 
     message_text = event.message.message.strip()
     response = ""
-    user_was_mentioned = False
-    seen_user_ids = {}
 
     if message_text.lower().startswith("/id all"):
         # Get all users in the chat
@@ -27,35 +25,51 @@ async def handle_new_message(event: telethon.events.NewMessage.Event):
                 )
     else:
         # Check for mentions in the message
+        seen_ids = {}
+        user_was_mentioned = False
         mentioned_entities = event.message.entities
         if mentioned_entities:
             for entity in mentioned_entities:
+
                 if isinstance(entity, telethon.types.MessageEntityMention):
                     user_was_mentioned = True
                     username = event.message.raw_text[
-                        entity.offset : entity.offset + entity.length
+                        entity.offset + 1 : entity.offset + entity.length
                     ]
-                    user = await client.get_entity(username)
-                    if seen_user_ids.get(user.id):
-                        continue
-                    seen_user_ids[user.id] = True
-                    response += f"{"🤖" if user.bot else "👤"} {user.first_name} -> `{user.id}`\n"
+                    input_peer = await client.get_input_entity(username)
+                    if isinstance(input_peer, telethon.types.InputPeerUser):
+                        if seen_ids.get(input_peer.user_id):
+                            continue
+                        seen_ids[input_peer.user_id] = True
+                        response += f"👤 {username} -> `{input_peer.user_id}`\n"
+                    elif isinstance(input_peer, telethon.types.InputPeerChat):
+                        if seen_ids.get(input_peer.chat_id):
+                            continue
+                        seen_ids[input_peer.chat_id] = True
+                        response += f"💬 {username} -> `{input_peer.chat_id}`\n"
+                    elif isinstance(input_peer, telethon.types.InputPeerChannel):
+                        if seen_ids.get(input_peer.channel_id):
+                            continue
+                        seen_ids[input_peer.channel_id] = True
+                        response += f"📢 {username} -> `{input_peer.channel_id}`\n"
+
                 elif isinstance(entity, telethon.types.MessageEntityMentionName):
                     user_was_mentioned = True
-                    user = await client.get_entity(entity.user_id)
-                    if seen_user_ids.get(user.id):
-                        continue
-                    seen_user_ids[user.id] = True
-                    if user.username:
-                        response += f"{"🤖" if user.bot else "👤"} {user.username} -> `{user.id}`\n"
-                    else:
-                        response += f"{"🤖" if user.bot else "👤"} {user.first_name} -> `{user.id}`\n"
+                    first_name = event.message.raw_text[
+                        entity.offset : entity.offset + entity.length
+                    ]
+                    input_peer = await client.get_input_entity(entity.user_id)
+                    if isinstance(input_peer, telethon.types.InputPeerUser):
+                        if seen_ids.get(input_peer.user_id):
+                            continue
+                        seen_ids[input_peer.user_id] = True
+                        response += f"👤 {first_name} -> `{input_peer.user_id}`\n"
 
         if not user_was_mentioned:
             # Default to sender's ID if no mentions
             response = f"👤 User ID: `{event.sender_id}`\n"
 
-    response += f"\n💬 Chat ID: `{event.chat_id}`"
+    response += f"\n💬 This chat ID: `{event.chat_id}`"
 
     if response:
         await event.reply(response)

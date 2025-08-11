fd5aeb4deb37d7cf6e292d7eab28488469f57eb9
m-ehsan <m_ehsan_sh@live.com>
2025-08-11 23:58:14 +0330

    minor refactor of failmeter module

--- utilities/failmeter/__init__.py
+++ utilities/failmeter/__init__.py
@@ -3,37 +3,37 @@ from collections import deque
 
 
 class FailMeter:
-    def __init__(self, count, window):
+    def __init__(self, threshold, window):
         """
-        Arguments
-            `count`: number of tolerable occurrences
+        Args:
+            `threshold`: number of tolerable occurrences
             `window`: time window (seconds)
         """
-        self._max_count = count
-        self._window = window
-        self._oc = deque()  # Occurrences
+        self.__threshold = threshold
+        self.__window = window
+        self.__oc = deque()  # Occurrences
 
     def trigger(self):
         """
         Report occurrence of failure.
 
-        Returns
+        Returns:
             `True` if the threshold is exceeded
             `False` if the threshold is not exceeded yet
         """
-        self._oc.append(int(time.time()))
-        return self._check()
+        self.__oc.append(int(time.time()))
+        return self.__check()
 
-    def _check(self):
-        self._cleanup()
-        if len(self._oc) > self._max_count:
+    def __check(self):
+        self.__cleanup()
+        if len(self.__oc) > self.__threshold:
             return True
         return False
 
-    def _cleanup(self):
+    def __cleanup(self):
         now = int(time.time())
         while True:
-            if self._oc[0] < now - self._window:
-                self._oc.popleft()
+            if self.__oc[0] < now - self.__window:
+                self.__oc.popleft()
             else:
                 break
